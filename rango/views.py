b37dc442ee9484017b53640f0fbdd27fb3323983
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-01-25 23:23:01 +0000

    Task 3 Completed

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpRespone("Rango says hey there partner!")
+
+    link_to_about = '<a href="/rango/about/">About</a>'
+    
+    return HttpResponse("Rango says hey there partner!" + link_to_about)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
+
+
 
 
