7c4617b6aafc332b415fed4b3de8b8994b6bc75c
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-02-06 17:20:54 +0000

    Still Fixing Categories Slugs

--- rango/views.py
+++ rango/views.py
@@ -1,21 +1,17 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
 from rango.models import Category
 from rango.models import Page
 
+from django.http import HttpResponse
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
+
     category_list = Category.objects.order_by('-likes')[:5]
+    pages = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
-    # Render the response and send it back!
+    context_dict['pages']= pages
     return render(request, 'rango/index.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -23,26 +19,19 @@ def show_category(request, category_name_slug):
 
     try:
         category = Category.objects.get(slug=category_name_slug)
-
         pages = Page.objects.filter(category=category)
 
         context_dict['pages'] = pages
-
         context_dict['category'] = category
-    except Category.DoesNotExist:
 
+    except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
+    
+    return render(request, 'rango/category.html', context=context_dict)
 
-    return render(request, 'rango/category.html', context_dict)
-
+    
 
 def about(request):
-
-    context_dict = {'boldmessage': 'This tutorial has been put together by Abdisalam'}
-
-    return render(request, 'rango/about.html', context=context_dict)
-
-
-
-
+    context = {'newmessage': 'This tutorial has been put together by Utkarsh Agrawal' }
+    return render(request, 'rango/about.html', context=context)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -5,18 +5,9 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('rango/', include('rango.urls')),
-    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-from rango.models import Category
-python_category = Category.objects.get(name="Python")  # Adjust if your category name is different
-print(python_category.slug)  # Check if the slug is set
-if not python_category.slug:
-    python_category.slug = 'python'  # Set an appropriate slug
-    python_category.save()
