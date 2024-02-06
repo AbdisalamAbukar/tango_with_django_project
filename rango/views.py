b5d15e445087cca3926d79ae37c256c78677e39d
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-02-06 17:38:38 +0000

    TASK 6 COMPLETED

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,11 @@ from rango.models import Page
 
 from django.http import HttpResponse
 def index(request):
-
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     pages = Page.objects.order_by('-views')[:5]
 
@@ -12,6 +16,12 @@ def index(request):
     context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages']= pages
+
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    #return render(request, 'rango/index.html', context=context_dict)
     return render(request, 'rango/index.html', context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -33,5 +43,5 @@ def show_category(request, category_name_slug):
     
 
 def about(request):
-    context = {'newmessage': 'This tutorial has been put together by Utkarsh Agrawal' }
+    context = {'newmessage': 'This tutorial has been put together by Abdisalam Abukar' }
     return render(request, 'rango/about.html', context=context)
