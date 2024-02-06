a5ae8d95211c86a8ad3c76caf487507bbc826c8c
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-02-06 16:29:46 +0000

    Task 6 Trying to Fix

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -12,3 +12,11 @@ urlpatterns = [
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+from rango.models import Category
+python_category = Category.objects.get(name="Python")  # Adjust if your category name is different
+print(python_category.slug)  # Check if the slug is set
+if not python_category.slug:
+    python_category.slug = 'python'  # Set an appropriate slug
+    python_category.save()
